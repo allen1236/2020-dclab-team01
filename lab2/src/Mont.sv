4c6f841880f0195ecd5019b56080e0f421993c82
b07901135 <b07901135@g.ntu.edu.tw>
2020-03-22 20:47:58 +0800

    lab2 init

---
+++ lab2/src/Mont.sv
@@ -0,0 +1,12 @@
+module Montgomery{
+	input			i_clk,
+	input			i_rst,
+	input			i_start,
+	input [255:0]	i_n,
+	input [255:0]	i_a,
+	input [255:0]	i_b,
+	output [255:0]	o_result,
+	output			o_finish
+};
+
+endmodule
--- lab2/src/Rsa256Core.sv
+++ lab2/src/Rsa256Core.sv
@@ -9,7 +9,28 @@ module Rsa256Core (
 	output         o_finished
 );
 
-// operations for RSA256 decryption
-// namely, the Montgomery algorithm
+/*========== States ==========*/
+parameter S_IDLE = 2'd0;
+parameter S_PREP = 2'd1;
+parameter S_MONT = 2'd2;
+parameter S_CALC = 2'd3;
+
+/*========== Parameters ==========*/
+
+/*========== Output Buffers ==========*/
+
+/*========== Variables ==========*/
+logic [1:0]		state_r, state_w;
+logic [255:0]	text_r, text_w;
+
+/*========== Output Assignments ==========*/
+
+/*========== Compinational Circuits ==========*/
+always_comb begin
+end
+
+/*========== Sequential Circuits ==========*/
+always_ff @(posedge i_clk or negedge i_rst_n) begin
+end
 
 endmodule
