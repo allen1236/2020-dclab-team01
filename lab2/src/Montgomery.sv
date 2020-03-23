44eb1a064cf54c32bff6067903f39fc8d8032acc
b07901135 <b07901135@g.ntu.edu.tw>
2020-03-23 21:46:59 +0800

    mont finished (without test)

--- lab2/src/Montgomery.sv
+++ lab2/src/Montgomery.sv
@@ -10,21 +10,69 @@ module Montgomery{
 };
 
 /*========== States ==========*/
+parameter S_IDLE = 2'd0;
+parameter S_CALC = 2'd1;
+parameter S_MINU = 2'd1;
 
 /*========== Parameters ==========*/
 
-/*========== Output Buffers ==========*/
-
 /*========== Variables ==========*/
+logic [1:0]		state_r, state_w;
+logic			finish_r, finish_w;
+logic [7:0]		index_r, index_w;
+logic [255:0]	m_r, m_w;
 
 /*========== Output Assignments ==========*/
+assign o_result = m_r;
+assign o_finish = finish_r;
 
 /*========== Compinational Circuits ==========*/
 always_comb begin
+
+	// default values
+	state_w		= state_r;
+	finish_w	= finish_r;
+	index_w		= index_r;
+	m_w			= m_r;
+
+	case( state_r ):
+		S_IDLE: begin
+			if ( i_start ) begin
+				state_w		= S_CALC;
+				finish_w	= 0;
+				index_w		= 0;
+				m_w			= 0;
+			end
+		end
+		S_CALC: begin
+			if ( i_a[index_r] ) begin 
+				m_w = ( i_b[0] ^ m_r[0] ) ? (m_r + i_b + N) >> 1 : (m_r + i_b) >> 1;
+			end
+			index_w		= index_r + 1;
+			state_w		= ( index_r == 8'd255 ) ? S_MINU : state_w;
+		end
+		S_MINU: begin
+			m_w = ( m_r > i_n )  ? m_r - i_n : m_w;
+			state_w  = S_IDLE;
+			finsi_w = 1'd1;
+		end
+	endcase
 end
 
 /*========== Sequential Circuits ==========*/
-always_ff @(posedge i_clk or negedge i_rst_n) begin
+always_ff @(posedge i_clk or posedge i_rst) begin
+	if ( i_rst  ) begin
+		state_r			<= S_IDLE;
+		finish_r		<= 1;
+		index_r			<= 0;
+		m_r				<= 0;
+	end
+	else
+		state_r			<= state_w;
+		finish_r		<= finish_w;
+		index_r			<= index_w;
+		m_r				<= m_w;
+	end
 end
 
 endmodule
