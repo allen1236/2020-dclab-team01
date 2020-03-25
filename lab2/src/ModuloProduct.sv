ace4cd5067c9654ec7d60eee6d28741829d907a1
WWFUG <bmpss95156@gmail.com>
2020-03-25 13:31:56 +0800

    ModuloProduct definition

--- lab2/src/ModuloProduct.sv
+++ lab2/src/ModuloProduct.sv
@@ -10,21 +10,30 @@ module ModuloProduct{
 };
 
 /*========== States ==========*/
-
+parameter S_IDLE = 1'd0;
+parameter S_CALC = 1'd1;
 /*========== Parameters ==========*/
+parameter [6:0] K = 7'd255;
 
 /*========== Output Buffers ==========*/
+logic[255:0] o_result_w, o_result_r;
+logic 		 o_finish_w, o_finish_w;
 
 /*========== Variables ==========*/
+logic [1:0] state_w, state_r;
+logic [6:0] counter_w, counter_r;
 
 /*========== Output Assignments ==========*/
+
+assign o_result = o_result_r;
+assign o_finish = o_finish_r;
 
 /*========== Compinational Circuits ==========*/
 always_comb begin
 end
 
 /*========== Sequential Circuits ==========*/
-always_ff @(posedge i_clk or negedge i_rst_n) begin
+always_ff @(posedge i_clk or posedge i_rst) begin
 end
 
 endmodule
