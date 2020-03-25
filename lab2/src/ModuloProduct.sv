9adc340a791ffe99848664f8fc02b9134f4023aa
WWFUG <bmpss95156@gmail.com>
2020-03-25 15:15:10 +0800

    ModuloProduct undebugged yet

--- lab2/src/ModuloProduct.sv
+++ lab2/src/ModuloProduct.sv
@@ -22,18 +22,66 @@ logic 		 o_finish_w, o_finish_w;
 /*========== Variables ==========*/
 logic [1:0] state_w, state_r;
 logic [6:0] counter_w, counter_r;
+logic [255:0] mult_w, mult_r;
 
 /*========== Output Assignments ==========*/
-
 assign o_result = o_result_r;
 assign o_finish = o_finish_r;
 
 /*========== Compinational Circuits ==========*/
 always_comb begin
+	// default value
+	o_result_w = o_result_r;
+	o_finish_w = o_finish_r;
+	state_w    = state_r;
+	mult_w     = mult_r;
+	counter_w  = counter_r;
+
+	case(state_r)
+
+	S_IDLE: begin
+		counter_w = 7'd0;
+		if(i_start) begin
+			sate_w = S_CALC;
+			mult_w = i_b;
+			o_result_w = 1'd0;
+		end
+	end
+
+	S_CALC: begin
+		counter_w = counter_r + 1;
+		if( counter_r == K) begin
+			state_w = S_IDLE;
+			o_finish_w = 1'd1;
+		end
+		else begin
+			if( i_a[counter_r]==1 ) begin
+				o_result_w = ( o_result_r+mult_r >= i_n) ? o_result_r+mult_r-i_n : o_result_r+i_n;
+			end
+		end
+		mult_w = (mult_r+mult_r > i_n) ? mult_r+mult_r-i_n : mult_r+mult_r;
+	end
+	endcase
+	
 end
 
 /*========== Sequential Circuits ==========*/
 always_ff @(posedge i_clk or posedge i_rst) begin
+
+if(i_rst) begin
+	counter_r  <= 0;
+	state_r    <= S_IDLE;
+	mult_r     <= 0;
+	o_result_r <= 0;
+	o_finish_r <= 0;
+end
+else begin
+	counter_r  <= counter_w;
+	state_r    <= state_w;
+	mult_r     <= mult_w;
+	o_result_r <= o_result_w;
+	o_finish_r <= o_finish_w;
+
 end
 
 endmodule
