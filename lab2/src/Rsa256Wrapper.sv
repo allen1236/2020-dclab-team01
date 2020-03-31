b28d1b431bec1b70261f26dbf1735b1eae1e3661
allen1236 <b07901135@ntu.edu.tw>
2020-03-31 17:59:03 +0800

    state undone

--- lab2/src/Rsa256Wrapper.sv
+++ lab2/src/Rsa256Wrapper.sv
@@ -16,10 +16,12 @@ localparam TX_OK_BIT   = 6;
 localparam RX_OK_BIT   = 7;
 
 // Feel free to design your own FSM!
-localparam S_GET_KEY = 0;
-localparam S_GET_DATA = 1;
-localparam S_WAIT_CALCULATE = 2;
-localparam S_SEND_DATA = 3;
+localparam S_RX_QUERY = 3'd0;
+localparam S_READ = 3'd1;
+localparam S_TX_QUERY = 3'd2;
+localparam S_WRITE = 3'd3;
+localparam S_CALC = 3'd4;
+localparam S_READ_BUFF = 3'd5;
 
 logic [255:0] n_r, n_w, d_r, d_w, enc_r, enc_w, dec_r, dec_w;
 logic [1:0] state_r, state_w;
@@ -49,25 +51,6 @@ Rsa256Core rsa256_core(
     .o_finished(rsa_finished)
 );
 
-//===== Tasks =====
-
-task StartRead;
-    input [4:0] addr;
-    begin
-        avm_read_w = 1;
-        avm_write_w = 0;
-        avm_address_w = addr;
-    end
-endtask
-task StartWrite;
-    input [4:0] addr;
-    begin
-        avm_read_w = 0;
-        avm_write_w = 1;
-        avm_address_w = addr;
-    end
-endtask
-
 
 always_comb begin
     n_w = n_r;
@@ -81,22 +64,62 @@ always_comb begin
     bytes_counter_w = bytes_counter_r;
     rsa_start_w = rsa_start_r;
 
-    case (state_r)
-        S_GET_KEY: begin
-
+    case (state_r) begin
+        S_RX_QUERY: begin
+            avm_address = STATUS_BASE;
+            avm_read = 1;
+            avm_write = 0;
         end
-
-        S_GET_DATA: begin
+        S_READ: begin
+            avm_address = RX_BASE;
+            avm_read = 1;
+            avm_write = 0;
         end
-
-        S_WAIT_CALCULATE: begin
-            
+        S_READ_BUFF: begin
+            avm_address = RX_BASE;
+            avm_read = 0
+            avm_write = 0
         end
-        
-        S_SEND_DATA: begin
-            
+        S_TX_QUERY: begin
+            avm_address = STATUS_BASE;
+            avm_read = 1;
+            avm_write = 0
+        end
+        S_WRITE: begin
+            avm_address = TX_BASE;
+            avm_read = 0
+            avm_write = 1;
+        end
+        S_CALC: begin
+            avm_address_w = STATUS_BASE;
+            avm_read_w = 0
+            avm_write_w = 0;
         end
     endcase
+
+    if ( !avm_waitrequest ) begin
+        case ( state_r )
+            S_RX_QUERY: begin
+                if ( avm_readdata[RX_OK_BIT] ) begin
+                    state_w = S_READ;
+                end
+            end
+            S_READ: begin
+            end
+            S_READ_BUFF: begin
+                state_w = S_RX_QUERY;
+            end
+            S_TX_QUERY: begin
+                if ( avm_readdata[TX_OK_BIT] ) begin
+                    state_w = S_WRITE;
+                end
+            end
+            S_WRITE: begin
+            end
+            S_CALC: begin
+            end
+        endcase
+    end
 
 
 
